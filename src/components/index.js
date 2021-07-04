cd31baf7fbdbac8d3581fd5c5e3eb379ef9b5f5e
tuphan <tuphan@descartes.network>
2021-07-04 17:54:23 +0700

    tween-one

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
+// Components
 export * from 'antd';
 export { default as Widget } from './widget';
 export { default as Brand } from './brand';
-export { default as Animate } from './animate';
+// Icons
 export { default as Icon } from './icon';
+// Animation
+export { default as Animate } from './animate';
+export { default as TweenOne } from 'rc-tween-one';
